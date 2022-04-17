1c6c4f8620886427e26e17b340300eb40d0c6cfa
FIRAS CHAHINE <firas@adaptivefort.com>
2022-04-17 16:42:32 -0400

    Complete exercise 4

--- Chapter 3 - Objects, Types, and Values/Exercise 2/main.cpp
+++ Chapter 3 - Objects, Types, and Values/Exercise 2/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+//Exercise 2
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double miles;
+    std::cout << "Welcome to miles to kilometers convert, please enter value for miles: ";
+    while (cin >> miles){
+        cout << static_cast<double>(miles) << " Equal to " << (miles * 1.609) << "KMs";
+    }
+
     return 0;
 }
---
+++ Chapter 3 - Objects, Types, and Values/Exercise 3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int double {0};
+    double int {0};
+    String char;
+    Int x = 7;
+    return 0;
+}
